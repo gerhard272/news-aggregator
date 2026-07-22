6205c761c60dc3370850157feddf3e026b4b8643
gerhard272 <gerhardpirretti2003@gmail.com>
2026-07-22 12:51:38 +0200

    Added services

--- src/app/services/notizie.ts
+++ src/app/services/notizie.ts
@@ -1,4 +1,42 @@
-import { Service } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { Articolo } from '../models/articolo';
 
-@Service()
-export class Notizie {}
+@Injectable({ providedIn: 'root' })
+export class Notizie {
+  private readonly http = inject(HttpClient);
+  private readonly url = 'assets/data/articoli.json';
+
+  getArticoli(): Observable<Articolo[]> {
+    return this.http.get<Articolo[]>(this.url);
+  }
+
+  getArticoloById(id: number): Observable<Articolo | undefined> {
+    return this.http
+      .get<Articolo[]>(this.url)
+      .pipe(map((articoli) => articoli.find((a) => a.id === id)));
+  }
+
+  getArticoliPerCategoria(categoria: string): Observable<Articolo[]> {
+    return this.http
+      .get<Articolo[]>(this.url)
+      .pipe(map((articoli) => articoli.filter((a) => a.categoria === categoria)));
+  }
+
+  getArticoliPerAutore(autoreId: number): Observable<Articolo[]> {
+    return this.http
+      .get<Articolo[]>(this.url)
+      .pipe(map((articoli) => articoli.filter((a) => a.autoreId === autoreId)));
+  }
+
+  getArticoliOrdinatiPerData(): Observable<Articolo[]> {
+    return this.http
+      .get<Articolo[]>(this.url)
+      .pipe(
+        map((articoli) =>
+          [...articoli].sort((a, b) => new Date(b.data).getTime() - new Date(a.data).getTime()),
+        ),
+      );
+  }
+}
