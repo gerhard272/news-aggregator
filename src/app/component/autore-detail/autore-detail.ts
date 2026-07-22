f6c924970fe13ce439657d3d0fc92795ab05affd
Aletuber00 <aletuber123@gmail.com>
2026-07-22 15:24:23 +0200

    feat: implementata pagina autori e dettagli autore

--- src/app/component/autore-detail/autore-detail.ts
+++ src/app/component/autore-detail/autore-detail.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Autori as AutoriService } from '../../services/autori';
+import { Notizie as NotizieService } from '../../services/notizie';
+import { Preferiti as PreferitiService } from '../../services/preferiti';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
+import { ArticoloCard } from '../shared/articolo-card/articolo-card';
+import { SlicePipe } from '@angular/common';
+
+import { Articolo } from '../../models/articolo';
 
 @Component({
   selector: 'app-autore-detail',
-  imports: [],
+  imports: [ArticoloCard, RouterLink, SlicePipe],
   templateUrl: './autore-detail.html',
   styleUrl: './autore-detail.css',
 })
-export class AutoreDetail {}
+export class AutoreDetail {
+  private route = inject(ActivatedRoute);
+  private autoriService = inject(AutoriService);
+  private notizieService = inject(NotizieService);
+
+  // Recupero l'autore
+  autore = toSignal(
+    this.route.paramMap.pipe(
+      switchMap((params) => {
+        const id = Number(params.get('id'));
+        return this.autoriService.getAutoreById(id);
+      })
+    )
+  );
+
+  // Recupero gli articoli dell'autore
+  articoli = toSignal(
+    this.route.paramMap.pipe(
+      switchMap((params) => {
+        const id = Number(params.get('id'));
+        return this.notizieService.getArticoliPerAutore(id);
+      })
+    ),
+    { initialValue: [] }
+  );
+
+  preferitiService = inject(PreferitiService);
+
+  onTogglePreferito(articolo: Articolo) {
+    this.preferitiService.toggle(articolo);
+  }
+}
+
--- src/app/component/autori/autori.ts
+++ src/app/component/autori/autori.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { Autori as AutoriService } from '../../services/autori';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-autori',
-  imports: [],
+  imports: [RouterOutlet, RouterLink, SlicePipe],
   templateUrl: './autori.html',
   styleUrl: './autori.css',
 })
-export class Autori {}
+export class Autori {
+  private autoriService = inject(AutoriService);
+  autori = toSignal(this.autoriService.getAutori(), { initialValue: [] });
+}
+
